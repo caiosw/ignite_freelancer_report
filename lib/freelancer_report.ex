e76446d6d2bb521ecb85be3d2413ec92b6613684
caiosw <caiosw@gmail.com>
2021-03-10 01:52:10 -0300

    new project init

---
+++ lib/freelancer_report.ex
@@ -0,0 +1,18 @@
+defmodule FreelancerReport do
+  @moduledoc """
+  Documentation for `FreelancerReport`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FreelancerReport.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/freelancer_report_test.exs
@@ -0,0 +1,8 @@
+defmodule FreelancerReportTest do
+  use ExUnit.Case
+  doctest FreelancerReport
+
+  test "greets the world" do
+    assert FreelancerReport.hello() == :world
+  end
+end
